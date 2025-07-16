38b73354ff2025ded734b93be274ecc363531da2
Rober44 <robertojrr02@gmail.com>
2025-07-16 16:28:19 -0400

    models cambio de imageField

--- core/models.py
+++ core/models.py
@@ -1,10 +1,25 @@
 from django.db import models
+from django.conf import settings
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+    
+def get_image_field():
+    if settings.DEBUG:
+        return models.ImageField(upload_to="products")
+    else:
+        return CloudinaryField("image")
+    
+def get_image_category_field():
+    if settings.DEBUG:
+        return models.ImageField(upload_to="categories")
+    else:
+        return CloudinaryField("image")
 
 class Category(models.Model):
     category = models.CharField(max_length=50)
-    image = models.ImageField(upload_to="categories", default="categories/defecto.jpeg")
+    image = get_image_category_field()
+    
 
     def __str__(self):
         return self.category
@@ -12,7 +27,7 @@ class Category(models.Model):
 class Products(models.Model):
     name = models.CharField(max_length=200)
     price = models.DecimalField(decimal_places=2, max_digits=20)
-    image = models.ImageField(upload_to="products")
+    image = get_image_field()
     details = models.TextField(blank=True, null=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     amount = models.IntegerField(default=1)
